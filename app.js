544f5c7ae33ca25a15d439325ae4688652c2fb4b
mf <myemail@isawesome.com>
2017-04-13 17:20:34 -0500

    Lab 11 - added Procfile

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ var users = require('./routes/users');
 
 var app = express();
 
-var url = 'mongodb://localhost:27017/garden';
+// var url = 'mongodb://localhost:27017/garden';
+var url = process.env.MONGO_URL;
 MongoClient.connect(url , function(err, db){
     {  assert.equal(null, err);
         console.log('connected to MongoDB');
